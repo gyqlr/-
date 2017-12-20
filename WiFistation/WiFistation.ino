1ccd7f81fd0d9cf4506052c23c4ce03d6e9c02cd
gyqlr <33630610+gyqlr@users.noreply.github.com>
2017-12-20 14:06:12 +0800

    Add files via upload

---
+++ WiFistation/WiFistation.ino
@@ -0,0 +1,234 @@
+#include <Adafruit_NeoPixel.h>//引用头文件
+#define PIN 6   /*定义了控制LED的引脚，6表示Microduino的D6引脚，可通过Hub转接出来，用户可以更改 */
+#define PIN_NUM 3 //定义允许接的led灯的个数
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIN_NUM, PIN, NEO_GRB + NEO_KHZ800); //该函数第一个参数控制串联灯的个数，第二个是控制用哪个pin脚输出，第三个显示颜色和变化闪烁频
+#define INTERVAL_SENSOR   17000             //定义传感器采样时间间隔  597000
+#define INTERVAL_NET      17000             //定义发送时间
+//传感器部分================================   
+#include <Wire.h>                                  //调用库  
+#include "./ESP8266.h"
+#include "I2Cdev.h"                                //调用库  
+//温湿度   
+#include "./SHT2x.h"
+//光照
+#define  sensorPin_1  A0
+
+#include <U8glib.h>//使用OLED需要包含这个头文件
+#define INTERVAL_LCD             20             //定义OLED刷新时间间隔  
+unsigned long lcd_time = millis();                 //OLED刷新时间计时器
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NONE);     //设置OLED型号  
+//-------字体设置，大、中、小
+#define setFont_L u8g.setFont(u8g_font_7x13)
+#define setFont_M u8g.setFont(u8g_font_fixed_v0r)
+#define setFont_S u8g.setFont(u8g_font_fixed_v0r)
+#define setFont_SS u8g.setFont(u8g_font_fub25n)
+long previousMillis = 0;        // 存储LED最后一次的更新
+long interval = 5000;           // 闪烁的时间间隔（毫秒）
+
+#define SSID           "PRA-AL00X"                   // cannot be longer than 32 characters!
+#define PASSWORD       "19990912"
+
+#define IDLE_TIMEOUT_MS  3000      // Amount of time to wait (in milliseconds) with no data 
+                                   // received before closing the connection.  If you know the server
+                                   // you're accessing is quick to respond, you can reduce this value.
+
+//WEBSITE     
+#define HOST_NAME   "api.heclouds.com"
+#define DEVICEID   "20408770"
+#define PROJECTID "104260"
+#define HOST_PORT   (80)
+String apiKey="jw41=Zoj1m1BfaiqI=TLPbs=XDM=";
+char buf[10];
+
+#define INTERVAL_sensor 2000
+unsigned long sensorlastTime = millis();
+
+float tempOLED, humiOLED, lightnessOLED;
+
+#define INTERVAL_OLED 1000
+
+String mCottenData;
+String jsonToSend;
+
+//3,传感器值的设置 
+float sensor_tem, sensor_hum, sensor_lux;                    //传感器温度、湿度、光照   
+char  sensor_tem_c[7], sensor_hum_c[7], sensor_lux_c[7] ;    //换成char数组传输
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(2, 3); /* RX:D3, TX:D2 */
+ESP8266 wifi(mySerial);
+//ESP8266 wifi(Serial1);                                      //定义一个ESP8266（wifi）的对象
+unsigned long net_time1 = millis();                          //数据上传服务器时间
+unsigned long sensor_time = millis();                        //传感器采样时间计时器
+
+//int SensorData;                                   //用于存储传感器数据
+String postString;                                //用于存储发送数据的字符串
+//String jsonToSend;                                //用于存储发送的json格式参数
+
+void setup(void)     //初始化函数  
+{   strip.begin();
+  //初始化串口波特率  
+    Wire.begin();
+    Serial.begin(115200);   
+    while(!Serial);
+    pinMode(sensorPin_1, INPUT);
+
+   //ESP8266初始化
+    Serial.print("setup begin\r\n");   
+
+  Serial.print("FW Version:");
+  Serial.println(wifi.getVersion().c_str());
+
+  if (wifi.setOprToStationSoftAP()) {
+    Serial.print("to station + softap ok\r\n");
+  } else {
+    Serial.print("to station + softap err\r\n");
+  }
+
+  if (wifi.joinAP(SSID, PASSWORD)) {      //加入无线网
+    Serial.print("Join AP success\r\n");  
+    Serial.print("IP: ");
+    Serial.println(wifi.getLocalIP().c_str());
+  } else {
+    Serial.print("Join AP failure\r\n");
+  }
+
+  if (wifi.disableMUX()) {
+    Serial.print("single ok\r\n");
+  } else {
+    Serial.print("single err\r\n");
+  }
+
+  Serial.print("setup end\r\n");
+    
+  
+}
+void loop(void)     //循环函数  
+{   
+  if (sensor_time > millis())  sensor_time = millis();  
+    
+  if(millis() - sensor_time > INTERVAL_SENSOR)              //传感器采样时间间隔  
+  {  
+    getSensorData();                                        //读串口中的传感器数 
+    sensor_time = millis();
+  }  
+
+
+  
+  if (net_time1 > millis())  net_time1 = millis();
+  
+  if (millis() - net_time1 > INTERVAL_NET)                  //发送数据时间间隔
+  {                
+    updateSensorData();                                     //将数据上传到服务器的函数
+    net_time1 = millis();
+  }
+  if (sensor_lux<50)
+  {strip.setPixelColor(0, strip.Color(0, 0, 0));
+  strip.show();//温度
+  strip.setPixelColor(1, strip.Color(0, 0, 0));
+  strip.show();//湿度
+  strip.setPixelColor(2, strip.Color(0, 0, 0));
+  strip.show();}//光照
+  
+    else 
+    {if (sensor_tem>10&&sensor_tem<25)
+  {strip.setPixelColor(0, strip.Color(0, 255, 0));
+  strip.show();}//绿色 温度正常
+  else 
+  {strip.setPixelColor(0, strip.Color(255, 0, 0));
+  strip.show();}//红色    温度异常
+ if (sensor_hum>40&&sensor_hum<80)
+  {strip.setPixelColor(1, strip.Color(0, 255, 0));
+  strip.show();}//绿色    湿度正常
+ else  
+  {strip.setPixelColor(1, strip.Color(255, 0, 0));
+  strip.show();}//红色    湿度异常
+  if (sensor_lux>600&&sensor_lux<30000)
+  {strip.setPixelColor(2, strip.Color(0,255 ,0 ));
+  strip.show();}//绿色    光照正常
+  else 
+  {strip.setPixelColor(2, strip.Color(255,0 , 0));
+  strip.show();}}//红色    光照异常
+
+u8g.firstPage();
+      do {
+          setFont_S;
+           u8g.setPrintPos(0, 10);
+          u8g.print("temp:");
+          u8g.setPrintPos(35, 10);
+          u8g.print(sensor_tem,HEX);
+          u8g.setPrintPos(0,30);
+          u8g.print("humid:");
+          u8g.setPrintPos(35,30);
+          u8g.print(sensor_hum,HEX);
+          u8g.setPrintPos(0,60);
+          u8g.print("light:");
+          u8g.setPrintPos(35,60);
+          u8g.print(sensor_lux,HEX);
+      }while( u8g.nextPage() );
+ 
+}
+
+void getSensorData(){  
+    sensor_tem = SHT2x.GetTemperature() ;   
+    sensor_hum = SHT2x.GetHumidity();   
+    //获取光照
+    sensor_lux = analogRead(A0);    
+    delay(1000);
+    dtostrf(sensor_tem, 2, 1, sensor_tem_c);
+    dtostrf(sensor_hum, 2, 1, sensor_hum_c);
+    dtostrf(sensor_lux, 3, 1, sensor_lux_c);
+    
+}
+void updateSensorData() {
+  if (wifi.createTCP(HOST_NAME, HOST_PORT)) { //建立TCP连接，如果失败，不能发送该数据
+    Serial.print("create tcp ok\r\n");
+
+jsonToSend="{\"Temperature\":";
+    dtostrf(sensor_tem,1,2,buf);
+    jsonToSend+="\""+String(buf)+"\"";
+    jsonToSend+=",\"Humidity\":";
+    dtostrf(sensor_hum,1,2,buf);
+    jsonToSend+="\""+String(buf)+"\"";
+    jsonToSend+=",\"Light\":";
+    dtostrf(sensor_lux,1,2,buf);
+    jsonToSend+="\""+String(buf)+"\"";
+    jsonToSend+="}";
+
+
+
+    postString="POST /devices/";
+    postString+=DEVICEID;
+    postString+="/datapoints?type=3 HTTP/1.1";
+    postString+="\r\n";
+    postString+="api-key:";
+    postString+=apiKey;
+    postString+="\r\n";
+    postString+="Host:api.heclouds.com\r\n";
+    postString+="Connection:close\r\n";
+    postString+="Content-Length:";
+    postString+=jsonToSend.length();
+    postString+="\r\n";
+    postString+="\r\n";
+    postString+=jsonToSend;
+    postString+="\r\n";
+    postString+="\r\n";
+    postString+="\r\n";
+
+  const char *postArray = postString.c_str();                 //将str转化为char数组
+  Serial.println(postArray);
+  wifi.send((const uint8_t*)postArray, strlen(postArray));    //send发送命令，参数必须是这两种格式，尤其是(const uint8_t*)
+  Serial.println("send success");   
+     if (wifi.releaseTCP()) {                                 //释放TCP连接
+        Serial.print("release tcp ok\r\n");
+        } 
+     else {
+        Serial.print("release tcp err\r\n");
+        }
+      postArray = NULL;                                       //清空数组，等待下次传输数据
+  
+  } else {
+    Serial.print("create tcp err\r\n");
+  }
+
+}
+
